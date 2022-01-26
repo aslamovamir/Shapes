046b97df4fccb207d21cea8f255f1e5d7d1457d4
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2022-01-25 23:56:22 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace AreaOfShapes
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.Triangle = new System.Windows.Forms.Label();
+            this.traingle_side1 = new System.Windows.Forms.TextBox();
+            this.triangle_side2 = new System.Windows.Forms.TextBox();
+            this.triangle_side3 = new System.Windows.Forms.TextBox();
+            this.Rectangle = new System.Windows.Forms.Label();
+            this.rectangle_length = new System.Windows.Forms.TextBox();
+            this.radius1 = new System.Windows.Forms.TextBox();
+            this.rectangle_width = new System.Windows.Forms.TextBox();
+            this.triangle_area = new System.Windows.Forms.TextBox();
+            this.circle_area1 = new System.Windows.Forms.TextBox();
+            this.rectangle_area1 = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Circle = new System.Windows.Forms.Label();
+            this.side1 = new System.Windows.Forms.Label();
+            this.side3 = new System.Windows.Forms.Label();
+            this.side2 = new System.Windows.Forms.Label();
+            this.length = new System.Windows.Forms.Label();
+            this.width = new System.Windows.Forms.Label();
+            this.radius = new System.Windows.Forms.Label();
+            this.traingle_area = new System.Windows.Forms.Label();
+            this.rectangle_area = new System.Windows.Forms.Label();
+            this.circle_area = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Triangle
+            // 
+            this.Triangle.AutoSize = true;
+            this.Triangle.Location = new System.Drawing.Point(62, 48);
+            this.Triangle.Name = "Triangle";
+            this.Triangle.Size = new System.Drawing.Size(45, 13);
+            this.Triangle.TabIndex = 0;
+            this.Triangle.Text = "Triangle";
+            // 
+            // traingle_side1
+            // 
+            this.traingle_side1.Location = new System.Drawing.Point(171, 41);
+            this.traingle_side1.Name = "traingle_side1";
+            this.traingle_side1.Size = new System.Drawing.Size(100, 20);
+            this.traingle_side1.TabIndex = 1;
+            this.traingle_side1.Text = "3";
+            // 
+            // triangle_side2
+            // 
+            this.triangle_side2.Location = new System.Drawing.Point(376, 41);
+            this.triangle_side2.Name = "triangle_side2";
+            this.triangle_side2.Size = new System.Drawing.Size(100, 20);
+            this.triangle_side2.TabIndex = 2;
+            this.triangle_side2.Text = "2";
+            // 
+            // triangle_side3
+            // 
+            this.triangle_side3.Location = new System.Drawing.Point(567, 41);
+            this.triangle_side3.Name = "triangle_side3";
+            this.triangle_side3.Size = new System.Drawing.Size(100, 20);
+            this.triangle_side3.TabIndex = 3;
+            this.triangle_side3.Text = "5";
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.AutoSize = true;
+            this.Rectangle.Location = new System.Drawing.Point(62, 149);
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(56, 13);
+            this.Rectangle.TabIndex = 4;
+            this.Rectangle.Text = "Rectangle";
+            // 
+            // rectangle_length
+            // 
+            this.rectangle_length.Location = new System.Drawing.Point(171, 152);
+            this.rectangle_length.Name = "rectangle_length";
+            this.rectangle_length.Size = new System.Drawing.Size(100, 20);
+            this.rectangle_length.TabIndex = 6;
+            // 
+            // radius1
+            // 
+            this.radius1.Location = new System.Drawing.Point(171, 261);
+            this.radius1.Name = "radius1";
+            this.radius1.Size = new System.Drawing.Size(100, 20);
+            this.radius1.TabIndex = 7;
+            this.radius1.Text = "23";
+            // 
+            // rectangle_width
+            // 
+            this.rectangle_width.Location = new System.Drawing.Point(376, 149);
+            this.rectangle_width.Name = "rectangle_width";
+            this.rectangle_width.Size = new System.Drawing.Size(100, 20);
+            this.rectangle_width.TabIndex = 8;
+            // 
+            // triangle_area
+            // 
+            this.triangle_area.Location = new System.Drawing.Point(171, 341);
+            this.triangle_area.Name = "triangle_area";
+            this.triangle_area.Size = new System.Drawing.Size(100, 20);
+            this.triangle_area.TabIndex = 9;
+            // 
+            // circle_area1
+            // 
+            this.circle_area1.Location = new System.Drawing.Point(567, 341);
+            this.circle_area1.Name = "circle_area1";
+            this.circle_area1.Size = new System.Drawing.Size(100, 20);
+            this.circle_area1.TabIndex = 11;
+            // 
+            // rectangle_area1
+            // 
+            this.rectangle_area1.Location = new System.Drawing.Point(376, 341);
+            this.rectangle_area1.Name = "rectangle_area1";
+            this.rectangle_area1.Size = new System.Drawing.Size(100, 20);
+            this.rectangle_area1.TabIndex = 12;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(570, 386);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(97, 38);
+            this.Calculate.TabIndex = 13;
+            this.Calculate.Text = "calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Circle
+            // 
+            this.Circle.AutoSize = true;
+            this.Circle.Location = new System.Drawing.Point(62, 264);
+            this.Circle.Name = "Circle";
+            this.Circle.Size = new System.Drawing.Size(33, 13);
+            this.Circle.TabIndex = 14;
+            this.Circle.Text = "Circle";
+            // 
+            // side1
+            // 
+            this.side1.AutoSize = true;
+            this.side1.Location = new System.Drawing.Point(201, 9);
+            this.side1.Name = "side1";
+            this.side1.Size = new System.Drawing.Size(35, 13);
+            this.side1.TabIndex = 15;
+            this.side1.Text = "side 1";
+            // 
+            // side3
+            // 
+            this.side3.AutoSize = true;
+            this.side3.Location = new System.Drawing.Point(593, 9);
+            this.side3.Name = "side3";
+            this.side3.Size = new System.Drawing.Size(35, 13);
+            this.side3.TabIndex = 16;
+            this.side3.Text = "side 3";
+            // 
+            // side2
+            // 
+            this.side2.AutoSize = true;
+            this.side2.Location = new System.Drawing.Point(409, 9);
+            this.side2.Name = "side2";
+            this.side2.Size = new System.Drawing.Size(35, 13);
+            this.side2.TabIndex = 17;
+            this.side2.Text = "side 2";
+            // 
+            // length
+            // 
+            this.length.AutoSize = true;
+            this.length.Location = new System.Drawing.Point(201, 112);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(36, 13);
+            this.length.TabIndex = 18;
+            this.length.Text = "length";
+            // 
+            // width
+            // 
+            this.width.AutoSize = true;
+            this.width.Location = new System.Drawing.Point(409, 112);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(32, 13);
+            this.width.TabIndex = 19;
+            this.width.Text = "width";
+            // 
+            // radius
+            // 
+            this.radius.AutoSize = true;
+            this.radius.Location = new System.Drawing.Point(201, 232);
+            this.radius.Name = "radius";
+            this.radius.Size = new System.Drawing.Size(35, 13);
+            this.radius.TabIndex = 20;
+            this.radius.Text = "radius";
+            // 
+            // traingle_area
+            // 
+            this.traingle_area.AutoSize = true;
+            this.traingle_area.Location = new System.Drawing.Point(186, 306);
+            this.traingle_area.Name = "traingle_area";
+            this.traingle_area.Size = new System.Drawing.Size(70, 13);
+            this.traingle_area.TabIndex = 21;
+            this.traingle_area.Text = "Triangle Area";
+            // 
+            // rectangle_area
+            // 
+            this.rectangle_area.AutoSize = true;
+            this.rectangle_area.Location = new System.Drawing.Point(383, 306);
+            this.rectangle_area.Name = "rectangle_area";
+            this.rectangle_area.Size = new System.Drawing.Size(81, 13);
+            this.rectangle_area.TabIndex = 22;
+            this.rectangle_area.Text = "Rectangle Area";
+            // 
+            // circle_area
+            // 
+            this.circle_area.AutoSize = true;
+            this.circle_area.Location = new System.Drawing.Point(593, 306);
+            this.circle_area.Name = "circle_area";
+            this.circle_area.Size = new System.Drawing.Size(58, 13);
+            this.circle_area.TabIndex = 23;
+            this.circle_area.Text = "Circle Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.circle_area);
+            this.Controls.Add(this.rectangle_area);
+            this.Controls.Add(this.traingle_area);
+            this.Controls.Add(this.radius);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.side2);
+            this.Controls.Add(this.side3);
+            this.Controls.Add(this.side1);
+            this.Controls.Add(this.Circle);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.rectangle_area1);
+            this.Controls.Add(this.circle_area1);
+            this.Controls.Add(this.triangle_area);
+            this.Controls.Add(this.rectangle_width);
+            this.Controls.Add(this.radius1);
+            this.Controls.Add(this.rectangle_length);
+            this.Controls.Add(this.Rectangle);
+            this.Controls.Add(this.triangle_side3);
+            this.Controls.Add(this.triangle_side2);
+            this.Controls.Add(this.traingle_side1);
+            this.Controls.Add(this.Triangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label Triangle;
+		private System.Windows.Forms.TextBox traingle_side1;
+		private System.Windows.Forms.TextBox triangle_side2;
+		private System.Windows.Forms.TextBox triangle_side3;
+		private System.Windows.Forms.Label Rectangle;
+		private System.Windows.Forms.TextBox rectangle_length;
+		private System.Windows.Forms.TextBox radius1;
+		private System.Windows.Forms.TextBox rectangle_width;
+		private System.Windows.Forms.TextBox triangle_area;
+		private System.Windows.Forms.TextBox circle_area1;
+		private System.Windows.Forms.TextBox rectangle_area1;
+		private System.Windows.Forms.Button Calculate;
+		private System.Windows.Forms.Label Circle;
+		private System.Windows.Forms.Label side1;
+		private System.Windows.Forms.Label side3;
+		private System.Windows.Forms.Label side2;
+		private System.Windows.Forms.Label length;
+		private System.Windows.Forms.Label width;
+		private System.Windows.Forms.Label radius;
+		private System.Windows.Forms.Label traingle_area;
+		private System.Windows.Forms.Label rectangle_area;
+		private System.Windows.Forms.Label circle_area;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaOfShapes
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Calculate_Click(object sender, EventArgs e)
+        {
+            //let's get all the values for the sides of the triangle
+            double side1, side2, side3;
+			side1 = double.Parse(traingle_side1.Text);
+			side2 = double.Parse(triangle_side2.Text);
+			side3 = double.Parse(triangle_side3.Text);
+
+			//now we calculate the area of the triangle
+			double area_triangle;
+			double s = (side1 + side2 + side3) / 2;
+			area_triangle = Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+			//set the value of the label for the area to the result of the calculation
+			triangle_area.Text = area_triangle.ToString();
+
+			//let's get all the values for the sides of the rectangle
+			double length, width;
+			length = double.Parse(rectangle_length.Text);
+			width = double.Parse(rectangle_width.Text);
+
+			//now we calculate the area of the rectangle
+			double area_rectangle = length * width;
+			//set the value of the label for the area to the result of the calculation
+			rectangle_area1.Text = area_rectangle.ToString();
+
+			//let's now get the value for the radius of the circle
+			double radius;
+			radius = double.Parse(radius1.Text);
+
+			//now we calculate the area of the circle
+			double circle_area = Math.PI * Math.Pow(radius, 2);
+			//set the value of the label for the area to the result of the calculation
+			circle_area1.Text = circle_area.ToString();
+		}
+	}
+}
